cdffa01f7a24bc288ae550cd7b15f3b831554e6c
Blackhart <manceau.thomas19@gmail.com>
2014-06-09 15:21:16 +0200

    victory condition of potention add

--- Project/Assets/Script/Potentio.cs
+++ Project/Assets/Script/Potentio.cs
@@ -3,8 +3,40 @@ using System.Collections;
 
 public class Potentio : Interaction
 {
+    public FmodEventAudioSource obj;
+    public string paramSound = "param01";
+    private int mWantedValue;
+
+    private void Start()
+    {
+        mWantedValue = Random.Range(0, 4);
+    }
+
+    public void VRAction()
+    {
+        Debug.Log("VRAction actived");
+        if (source != null)
+        {
+            source.Play();
+            this.UpdateValueObj();
+        }
+    }
+
+    private void UpdateValueObj()
+    {
+        float value;
+
+        if (obj == null)
+            return;
+        value = obj.GetParameterValue(paramSound);
+        value += 1;
+        obj.SetParameterValue(paramSound, Mathf.Clamp(value, obj.GetParameterMinRange(paramSound), obj.GetParameterMaxRange(paramSound)));
+    }
+
     public override bool victoryState()
     {
-        return base.victoryState();
+        //if (value == mWantedValue)
+        //  return true;
+        return false;
     }
 }
