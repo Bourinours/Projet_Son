df2685e01e84151bee87c90748b03a49128161c6
Blackhart <manceau.thomas19@gmail.com>
2014-06-09 15:53:10 +0200

    victory condition of potention update

--- Project/Assets/Script/Potentio.cs
+++ Project/Assets/Script/Potentio.cs
@@ -5,38 +5,57 @@ public class Potentio : Interaction
 {
     public FmodEventAudioSource obj;
     public string paramSound = "param01";
-    private int mWantedValue;
+    private float mPrimRot = 0.0f;
 
     private void Start()
     {
-        mWantedValue = Random.Range(0, 4);
+        float value = 0.0f;
+        while (value >= 4.0f && value <= 7.0f)
+            value = Random.Range(3.0f, 8.0f);
+        obj.SetParameterValue(paramSound, Mathf.Clamp(value, obj.GetParameterMinRange(paramSound), obj.GetParameterMaxRange(paramSound)));
+    }
+
+    private void Update()
+    {
+        if (mActionMutex)
+            return;
+        if (wandGrab == null)
+        {
+            mPrimRot = 0.0f;
+            return;
+        }
+        float lRot = wandGrab.transform.localRotation.z;
+        this.UpdateValueObj(lRot - mPrimRot);
+        obj.Play();
+        mPrimRot = lRot;
     }
 
     public void VRAction()
     {
+        if (mActionMutex)
+            return;
+        mPrimRot = wandGrab.transform.localRotation.z;
         Debug.Log("VRAction actived");
         if (source != null)
-        {
             source.Play();
-            this.UpdateValueObj();
-        }
     }
 
-    private void UpdateValueObj()
+    private void UpdateValueObj(float val)
     {
         float value;
 
         if (obj == null)
             return;
         value = obj.GetParameterValue(paramSound);
-        value += 1;
+        value += val;
         obj.SetParameterValue(paramSound, Mathf.Clamp(value, obj.GetParameterMinRange(paramSound), obj.GetParameterMaxRange(paramSound)));
     }
 
     public override bool victoryState()
     {
-        //if (value == mWantedValue)
-        //  return true;
+        float value = obj.GetParameterValue(paramSound);
+        if (value >= 4.0f && value <= 7.0f)
+            return true;
         return false;
     }
 }
