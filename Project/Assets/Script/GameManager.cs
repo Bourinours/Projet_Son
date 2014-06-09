c88bb5f75cded42ab234e1253bd1d9be944436b3
Blackhart <manceau.thomas19@gmail.com>
2014-06-09 18:41:29 +0200

    add etap manager to gameManager

--- Project/Assets/Script/GameManager.cs
+++ Project/Assets/Script/GameManager.cs
@@ -12,11 +12,22 @@ public class GameManager : MonoBehaviour {
     public List<FmodEvent> mEtap4;
     public List<FmodEvent> mEtap5;
     public List<FmodEvent> mEtap6;
+    private eState mState;
+    
     private bool mEnd = false;
+    private enum eState
+    {
+        _ETAP1_ = 0,
+        _ETAP2_,
+        _ETAP3_,
+        _ETAP4_,
+        _ETAP5_,
+        _ETAP6_
+    };
 
 	// Use this for initialization
 	void Start () {
-	
+        mState = eState._ETAP1_;
 	}
 	
 	// Update is called once per frame
@@ -29,19 +40,53 @@ public class GameManager : MonoBehaviour {
 
     private void updateState()
     {
+        if (mState == eState._ETAP1_)
+            etap1();
+        else if (mState == eState._ETAP2_)
+            etap2();
+        else if (mState == eState._ETAP3_)
+            etap3();
+        else if (mState == eState._ETAP4_)
+            etap4();
+        else if (mState == eState._ETAP5_)
+            etap5();
+        else if (mState == eState._ETAP6_)
+            etap6();
 //        source.SetSourceEvent(mEvent[0]);
+    }
+
+    private void etap1()
+    {
+
+    }
+
+    private void etap2()
+    {
+
+    }
+
+    private void etap3()
+    {
+
+    }
+
+    private void etap4()
+    {
+
+    }
+
+    private void etap5()
+    {
+
+    }
+
+    private void etap6()
+    {
+
     }
 
     private void end()
     {
         Debug.Log("End");
-    }
-
-    private void disactiveObjects()
-    {
-        for (int i = 0; i < mObjects.Count; i++)
-        {
-            mObjects[i].enabled = false;
-        }
     }
 }
